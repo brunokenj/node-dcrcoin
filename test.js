b12f87ecda2d6acdfc5b4bc11751716bb9eb7a12
Bruno Kenj <bruno.kenj@gmail.com>
2017-08-09 20:01:39 -0300

    add dcrd and dcrwallet in same client

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 var dcrcoin = require('./lib');
 var fs = require('fs');
 
+// dcrd client
 var dcrd = new dcrcoin.Client({
   host: '127.0.0.1',
   port: 19109, // testnet default port
   user: 'brunokenj',
   pass: 'J6BINJBX5pCnrr1P5RO87na3DNLPsvv/',
   ssl: true
-  //,sslCa: fs.readFileSync('~/.dcrd/rpc.cert')
+  ,sslCa: fs.readFileSync('~/.dcrd/rpc.cert')
 });
 
 dcrd.cmd('getinfo', function(err, info){
@@ -15,13 +16,14 @@ dcrd.cmd('getinfo', function(err, info){
   console.log('getinfo:', info);
 });
 
+// dcrwallet client
 var dcrwallet = new dcrcoin.Client({
   host: '127.0.0.1',
   port: 19110, // testnet wallet default port
   user: 'brunokenj',
   pass: 'J6BINJBX5pCnrr1P5RO87na3DNLPsvv/',
   ssl: true
-  //,sslCa: fs.readFileSync('~/.dcrd/rpc.cert')
+  ,sslCa: fs.readFileSync('~/.dcrd/rpc.cert')
 });
 
 var accountName = Date.now().toString();
@@ -35,6 +37,27 @@ dcrwallet.cmd('createnewaccount', accountName, function(err, wallets){
 });
 
 dcrwallet.cmd('listaccounts', function(err, wallets){
+  if (err) return console.log(err);
+  console.log('listaccounts:', wallets);
+});
+
+// Dcrd and DcrWallet in same Client
+var dcrdAndDcrWallet = new dcrcoin.Client({
+  host: '127.0.0.1',
+  dcrdPort: 19109, // testnet default port
+  dcrWalletPort: 19110, // testnet wallet default port
+  user: 'brunokenj',
+  pass: 'J6BINJBX5pCnrr1P5RO87na3DNLPsvv/',
+  ssl: true
+  //,sslCa: fs.readFileSync('~/.dcrd/rpc.cert')
+});
+
+dcrdAndDcrWallet.cmd('getinfo', function(err, info){
+  if (err) return console.log(err);
+  console.log('getinfo:', info);
+});
+
+dcrdAndDcrWallet.cmd('wallet', 'listaccounts', function(err, wallets){
   if (err) return console.log(err);
   console.log('listaccounts:', wallets);
 });
